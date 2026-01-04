aec44c40e244a38418d88ed8dbaac7602743b734
Rafa Ahamada Wijaya <rafa9011@gmail.com>
2026-01-04 17:27:25 +0700

    fix lint problems

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const app = express();
 const PORT = 9000;
@@ -8,16 +9,16 @@ app.use(express.json());
 app.use(timeout('10s'));
 
 app.route('/books')
-    .post((req, res) => addBook(req, res))
-    .get((req, res) => getAllBooks(req, res));
+  .post((req, res) => addBook(req, res))
+  .get((req, res) => getAllBooks(req, res));
 
 app.route('/books/:id')
-    .get((req, res) => getBookById(req, res))
-    .put((req, res) => updateBook(req, res))
-    .delete((req, res) => deleteBook(req, res));
+  .get((req, res) => getBookById(req, res))
+  .put((req, res) => updateBook(req, res))
+  .delete((req, res) => deleteBook(req, res));
 
 app.listen(PORT, () => {
-    if (process.env.npm_lifecycle_event === 'start-dev') {
-        console.log(`App succesfully run on PORT ${PORT}`);
-    }
-})
+  if (process.env.npm_lifecycle_event === 'start-dev') {
+    console.log(`App succesfully run on PORT ${PORT}`);
+  }
+});
